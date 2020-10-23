3e6e6fb6a3c6c0a1bca032d77b6f92b4ee30d9c6
TsigeredaTek <rose_ym@yahoo.com>
2020-10-23 09:04:36 -0500

    Routing in React

---
+++ src/components/home.jsx
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+
+
+
+
+class Home extends Component {
+
+ 
+    
+    render() {
+        return(
+            <Fragment>
+            <div className='mt-5'>
+            <h1>About Studio Ghibli</h1></div>
+            <div className='mt-5 d-flex justify-content-center'>
+            <div className='text-center w-75'>Studio Ghibli.
+            </div>
+            </div>
+            </Fragment>
+        );
+    }
+}
+
+
+
+export default Home;
